1eb3315d750cf23f46408ab4cb618f963421afb4
Alija Bhujel <166594975+alijabhujel@users.noreply.github.com>
2024-05-14 19:20:01 +0545

    Create localization.dart

---
+++ localization.dart
@@ -0,0 +1,5 @@
+ return MaterialApp(
+///   localizationsDelegates: AppLocalizations.localizationsDelegates,
+///   supportedLocales: AppLocalizations.supportedLocales,
+///   home: MyApplicationHome(),
+/// );
